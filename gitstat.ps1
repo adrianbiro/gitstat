381d0c037e511f5a121a7d7ce653e4104952a5e5
Adrián Bíro <biroadrian@protonmail.com>
2022-12-09 19:18:50 +0100

    config

--- gitstat.ps1
+++ gitstat.ps1
@@ -1,4 +1,19 @@
-#python3 $(Join-Path -Path $HOME -ChildPath "src\gitstat\gitstat.py")
+<#
+    .SYNOPSIS
+        Check if checksum is valid
+    
+    .DESCRIPTION  
+        
+    .NOTES
+        Recursively search for changes in git repos from the root directory.
+        If there is no default location for the git repos, it will run the setup 
+        and prompt the user to confirm the correct location. 
+        python3 $(Join-Path -Path $HOME -ChildPath "src\gitstat\gitstat.py")
+
+    .LINK
+        https://github.com/adrianbiro/gitstat
+        https://github.com/adrianbiro/winbin
+#>
 $oldPath = $PWD.Path
 function SetUpLocation () {
     $configFile = Join-Path -Path $HOME -ChildPath ".gitstat.xml"
@@ -12,14 +27,12 @@ function SetUpLocation () {
         }
     }
     return ([xml]$XmlDocument = Get-Content -Path $configFile).Location
-
-
 }
-#$location = Join-Path -Path $HOME -ChildPath "src"  # TODO config 
+
 $location = SetUpLocation
 $OutputEncoding = [Console]::OutputEncoding  # nejde more
-$owner = git config user.name | Out-String -NoNewline  #TODO  Adri├ín B├¡ro
-#$owner = $env:UserName
+$owner = git config user.name | Out-String -NoNewline  #TODO  Adri├ín B├¡ro 
+#$owner = $env:UserName #TODO add to xml
 
 
 "{0}`nStatus overview of local git repositories from: {1}`nOwned by {2}.`n" -f 
@@ -30,6 +43,7 @@ Get-ChildItem -Directory -Recurse $location |
     if ((Get-ChildItem -Hidden $_.FullName).Name -contains ".git" ) {
         Set-Location $_.FullName
         if (git status -s) {
+            $PWD.Path
             git status -sb
             [bool] $todo = 1
         }
