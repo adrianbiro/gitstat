79868ea9d53bbd53faff1f7ee5798d0f5058afb6
Adrian Biro <biroadrian@protonmail.com>
2023-01-17 12:46:51 +0100

    small fixes

--- gitstat.ps1
+++ gitstat.ps1
@@ -14,32 +14,39 @@
         https://github.com/adrianbiro/gitstat
         https://github.com/adrianbiro/winbin
 #>
-$oldPath = $PWD.Path
+
 function SetUpLocation () {
     $configFile = Join-Path -Path $HOME -ChildPath ".gitstat.xml"
     if ( -not (Test-Path $configFile)) {
         $str = "{0}{1}{2}" -f
-                "If this isn't the root level of your git repos run program again from that location:`n`t", 
-                $oldPath, "`nTo continue write yes. " 
+        "If this isn't the root level of your git repos run program again from that location:`n`t", 
+        $oldPath, "`nTo continue write yes. " 
         $Islocation = Read-Host $str
         if ($Islocation -match "yes") {
-            New-Item $configFile -ItemType File | % {Set-Content $_.FullName "<Location>$oldPath</Location>"}
+            New-Item $configFile -ItemType File | % { Set-Content $_.FullName "<Location>$oldPath</Location>" }
         }
     }
     return ([xml]$XmlDocument = Get-Content -Path $configFile).Location
 }
+function start-banner {
+    return ("{0}`nStatus overview of local git repositories from: {1}`nOwned by {2}.`n" -f 
+    (Get-Date -Format "dd/MM/yyyy HH:mm:ss"), $location, $owner)
+}
 
+
+if ( $OutputEncoding.EncodingName -ne 'Unicode (UTF-8)') {
+    $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+}
+
+$oldPath = $PWD.Path
 $location = SetUpLocation
-$OutputEncoding = [Console]::OutputEncoding  # nejde more
-$owner = git config user.name | Out-String -NoNewline  #TODO  Adri├ín B├¡ro 
+$owner = git config user.name | Out-String -NoNewline  #TODO  Adri├ín B├¡ro test this
 #$owner = $env:UserName #TODO add to xml
 
+Write-Host(start-banner)
 
-"{0}`nStatus overview of local git repositories from: {1}`nOwned by {2}.`n" -f 
-    (Get-Date -Format "dd/MM/yyyy HH:mm:ss"), $location, $owner
-
-Get-ChildItem -Directory -Recurse $location |
-    % {
+Get-ChildItem -Directory -Recurse $location |`
+    ForEach-Object {
     if ((Get-ChildItem -Hidden $_.FullName).Name -contains ".git" ) {
         Set-Location $_.FullName
         if (git status -s) {
@@ -47,7 +54,7 @@ Get-ChildItem -Directory -Recurse $location |
             git status -sb
             [bool] $todo = 1
         }
-   }
+    }
 }
-if (-not $todo) {"There is nothing to do."}  
+if (-not $todo) { "There is nothing to do." }  
 Set-Location $oldPath
